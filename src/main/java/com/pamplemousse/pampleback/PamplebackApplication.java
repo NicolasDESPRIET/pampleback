082c00307226d42b8660b72779e9aa74f9ae37dd
NicolasDESPRIET <nicolas.despriet@isen.yncrea.fr>
2021-11-24 11:45:41 +0100

    first commit

---
+++ src/main/java/com/pamplemousse/pampleback/PamplebackApplication.java
@@ -0,0 +1,13 @@
+package com.pamplemousse.pampleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamplebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamplebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pamplemousse/pampleback/PamplebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamplemousse.pampleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamplebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
